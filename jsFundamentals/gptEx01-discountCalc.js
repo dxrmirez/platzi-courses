e9ab80dce710f931f0613376e456d9541f0f1780
David Ramirez <dxrmirez@gmail.com>
2025-08-14 14:36:44 -0500

    add: exercise #1 of chatGPT

---
+++ jsFundamentals/gptEx01-discountCalc.js
@@ -0,0 +1,13 @@
+/* CALCULADORA DE DESCUENTOS
+Crea un programa que calcule el precio final de un producto después de aplicar un descuento.
+
+Logica:
+    - El usuario deberá ingresar el precio original.
+    - Luego ingresará el porcentaje de descuento.
+    - El resultado debe mostrarse en consola y en un alert() con un mensaje claro.
+Condiciones:
+    1. No aceptar precios negativos ni descuentos mayores al 100%.
+    2. El precio final debe tener máximo dos decimales.
+    3. Usar variables y operadores aritméticos. 
+*/
+
--- jsFundamentals/platziEx01-loginChallenge.js
+++ jsFundamentals/platziEx01-loginChallenge.js
@@ -1,10 +1,10 @@
-/* 
-Requerimientos del reto:
+/* RETO DE LOG-IN
 
-1. El usuario debe poder ingresar su usuario y contraseña
-2. El sistema debe ser capaz de validar si el usuario y contraseña ingresados por el usuario existen en la base de datos
-3. Si el usuario y contraseña son correctos, el sistema debe mostrar un mensaje de bienvenida y mostrar el timeline del usuario.
-4. Si el usuario y contraseña son incorrectos, el sistema debe mostrar un mensaje de error y no mostrar ningun timeline.
+Condiciones:
+    1. El usuario debe poder ingresar su usuario y contraseña
+    2. El sistema debe ser capaz de validar si el usuario y contraseña ingresados por el usuario existen en la base de datos
+    3. Si el usuario y contraseña son correctos, el sistema debe mostrar un mensaje de bienvenida y mostrar el timeline del usuario.
+    4. Si el usuario y contraseña son incorrectos, el sistema debe mostrar un mensaje de error y no mostrar ningun timeline.
 */
 
 const usersDatabase = [
@@ -40,8 +40,6 @@ const usersTimeline = [
         timeline: "I don't want to work today :(",
     },
 ];
-
-/* mySolution */
 
 const loginUsername = prompt("Enter your username: ")
 const loginPassword = prompt("Enter your password: ")
