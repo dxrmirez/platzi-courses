0eb0ca9b6efa036967c5c28e61ec8e2c1053f0b6
David Ramirez <dxrmirez@gmail.com>
2025-08-20 12:08:11 -0500

    new branch: clean

--- jsFundamentals/gptEx13-todoList.js
+++
@@ -1,9 +0,0 @@
-/* LISTA DE TAREAS EN EL DOM
-Haz una pequeña aplicación web que permita al usuario agregar tareas en una lista. Cada tarea debe mostrarse en pantalla y se agregará dinámicamente al hacer clic en un botón.
-
-Condiciones:
-    1. No permitir agregar tareas vacías.
-    2. Cada tarea se mostrará como un <li> dentro de la lista.
-*/
-
-//Este ejercicio se valida con el #11 y #12.
